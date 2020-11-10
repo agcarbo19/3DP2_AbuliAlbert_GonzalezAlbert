675427911e00a8de394f04b136976b4dc9585b65
aafederico <aabuli@edu.tecnocampus.cat>
2020-11-10 13:04:10 +0100

    FlipPanel

---
+++ Portal/Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public AudioSource m_ButtonDown;
+    public AudioSource m_ButtonUp;
+    public GameObject m_ActionGameObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<FPSController>())
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
+            m_ButtonDown.Play();
+            m_ActionGameObject.GetComponent<FlipPanel>().m_InAction = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponentInParent<FPSController>())
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+            m_ButtonUp.Play();
+            m_ActionGameObject.GetComponent<FlipPanel>().m_InAction = false;
+        }
+    }
+
+}
---
+++ Portal/Assets/Scripts/FlipPanel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class FlipPanel : MonoBehaviour
+{
+    public bool m_InAction;
+    private Vector3 m_PanelFlip = new Vector3(0f, 180f, 0f);
+    private Vector3 m_CurrentAngle;
+    private Vector3 m_InitialAngle;
+
+    private void Awake()
+    {
+        m_CurrentAngle = transform.eulerAngles;
+        m_InitialAngle = transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+        if (m_InAction)
+        {
+            m_CurrentAngle = new Vector3(
+             Mathf.LerpAngle(m_CurrentAngle.x, m_PanelFlip.x, Time.deltaTime),
+             Mathf.LerpAngle(m_CurrentAngle.y, m_PanelFlip.y, Time.deltaTime),
+             Mathf.LerpAngle(m_CurrentAngle.z, m_PanelFlip.z, Time.deltaTime));
+
+            transform.eulerAngles = m_CurrentAngle;
+        }
+        else
+        {
+            m_CurrentAngle = new Vector3(
+             Mathf.LerpAngle(m_CurrentAngle.x, m_InitialAngle.x, Time.deltaTime),
+             Mathf.LerpAngle(m_CurrentAngle.y, m_InitialAngle.y, Time.deltaTime),
+             Mathf.LerpAngle(m_CurrentAngle.z, m_InitialAngle.z, Time.deltaTime));
+
+            transform.eulerAngles = m_CurrentAngle;
+        }
+    }
+}
